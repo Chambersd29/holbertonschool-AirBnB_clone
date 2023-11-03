94d0190f5d5c4d03e6c4cf7d8dbb254c53158baf
Chambersd29 <chambers.git29@gmail.com>
2023-11-02 18:17:44 -0700

    03:Basemodel try1

--- models/base_model.py
+++ models/base_model.py
@@ -16,16 +16,16 @@ class BaseModel:
         """
         Este método inicializa lo sgte:
             id, created_at, updated_at
-            Attr:
-                id (str): Genera un id cada vez que se instancia.
-                created_ad (str): Genera la hora y fecha cada vez
-                        que se instancia un nuevo objeto.
-                updated_at (str): Genera y actualiza la hora y fecha,
-                        cada vez que se cambia nuestra instancia.
+        Attr:
+            id (str): Genera un id cada vez que se instancia.
+            created_ad (str): Genera la hora y fecha cada vez
+                    que se instancia un nuevo objeto.
+            updated_at (str): Genera y actualiza la hora y fecha,
+                    cada vez que se cambia nuestra instancia.
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -34,20 +34,22 @@ class BaseModel:
         Returns:
             str: Representación de instancia.
         """
-        return f"[BaseModel] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Este método actualiza la fecha de creación updated_at.
         """
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
         Este método regresa un diccionario.
-
+	
         Returns:
             dict: Regresa un diccionario con los atributos de instancia.
-        """
-        self.__dict__["__class__"] = "BaseModel"
+	"""
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
         return self.__dict__
